ac69fd3fb3db523819b7cef21aeaaf26c6c675ed
Eugene Brian B. Dela Cerna <99775995+Yuj9n@users.noreply.github.com>
2022-06-02 12:12:40 +0800

    Add files via upload

---
+++ Laboratory_No_8..py
@@ -0,0 +1,10 @@
+class Enter:
+    pass
+
+
+Enter the first number:123
+Enter the second number:52
+Enter the third number:-5
+The largest number among the three is: 123
+
+Process finished with exit code 0
